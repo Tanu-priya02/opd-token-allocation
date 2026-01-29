0655e38e66895a7b8939af6a9c60fb646c379172
Tanu-priya02 <121554388+Tanu-priya02@users.noreply.github.com>
2026-01-29 11:19:31 +0530

    .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': 'error'
+  }
+};
